4dcace0f49810274de5ee2e1091bdc0ac1a256eb
Adam Shields <adamenote@gmail.com>
2021-09-16 11:13:02 -0400

    Reset refactored now working on viewset for server-software-publisher

--- servers/api/urls.py
+++ servers/api/urls.py
@@ -6,6 +6,7 @@ from .views import (
     ServerListAPIView,
     ServerCreateAPIView,
     ServerDetailAPIView,
+    ServerUpdateAPIView,
     SoftwareListAPIView,
     SoftwareCreateAPIView,
     SoftwareDetailAPIView,
@@ -13,6 +14,7 @@ from .views import (
     PublisherCreateAPIView,
     PublisherDetailAPIView,
     server_software_publisher_api_home,
+    ServerAdamViewSet
 )
 from rest_framework import renderers
 from django.urls import path, include
@@ -25,6 +27,7 @@ router = routers.DefaultRouter()
 router.register(r"publishers", PublisherViewSet)
 router.register(r"software", SoftwareViewSet)
 router.register(r"servers", ServerViewSet)
+router.register(r"serveradam", ServerAdamViewSet)
 
 
 urlpatterns = [
@@ -32,14 +35,16 @@ urlpatterns = [
     path('', server_software_publisher_api_home),
     # Server
     path("server/", ServerListAPIView.as_view(), name="server_list_api"),
-    path("server/create", ServerCreateAPIView.as_view(), name="server_create_api"),
-    path("server/<slug:slug>", ServerDetailAPIView.as_view(), name="server_detail_api"),
+    path("server/create/", ServerCreateAPIView.as_view(), name="server_create_api"),
+    path("server/<slug:server_slug>/", ServerDetailAPIView.as_view(), name="server_detail_api"),
+    path("server/<slug:server_slug>/update/", ServerUpdateAPIView.as_view(), name="server_update_api"),
+    # path("server/<slug:server_slug>/", ServerDetailAPIView.as_view(), name="server_detail_api"),
     # Software
     path("software/", SoftwareListAPIView.as_view(), name="software_list_api"),
-    path("software/create", SoftwareCreateAPIView.as_view(), name="software_create_api"),
-    path("software/<slug:slug>", SoftwareDetailAPIView.as_view(), name="software_detail_api"),
+    path("software/create/", SoftwareCreateAPIView.as_view(), name="software_create_api"),
+    path("software/<slug:slug>/", SoftwareDetailAPIView.as_view(), name="software_detail_api"),
     path("publisher/", PublisherListAPIView.as_view(), name="publisher_list_api"),
     # Publisher
-    path("publisher/create", PublisherCreateAPIView.as_view(), name="publisher_create_api"),
+    path("publisher/create/", PublisherCreateAPIView.as_view(), name="publisher_create_api"),
     path("publisher/<slug:slug>", PublisherDetailAPIView.as_view(), name="publisher_detail_api"),
 ]
